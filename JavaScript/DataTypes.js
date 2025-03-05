29daae430ed32c7035f68af37446bbf9c0d794b5
B SHIVANAND <114202721+Shivanand694@users.noreply.github.com>
2025-03-05 12:28:09 +0000

    veriables

---
+++ JavaScript/DataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code in newer version 
+
+alert("Hello world!") // we are node js not browser
+
+// code readability should be very high 
+
+let name = 'SHIVANAND'
+let age = 33;
+let isLoggedin = false;
+
+// data types 
+// numbers => 2 to power 53
+// bigint  => 
+// string = "Shiv"
+// boolean => true/false
+// null  standalone value
+// undefined 
+// symbol 
+// objects 
+
+console.log(typeof 'Shivanand') // output string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
---
+++ JavaScript/Variables.js
@@ -0,0 +1,8 @@
+console.log("hello Shiv");
+const ID = 12354658;
+let acId = "shivanand@gmail.com"
+let city = "Hyderabad"
+let otherCity = 'BIDAR'
+let others;
+
+console.table([ID,acId,city,otherCity,others]);
